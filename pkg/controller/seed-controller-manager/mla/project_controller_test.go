8566f52321f57f0a3f212a83da01b28caaea5073
Aborilov Pavel <aborilov@gmail.com>
2021-03-24 13:27:55 +0300

    add project controller tests

--- pkg/controller/seed-controller-manager/mla/project_controller_test.go
+++ pkg/controller/seed-controller-manager/mla/project_controller_test.go
@@ -3,9 +3,11 @@ package mla
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 
@@ -22,12 +24,12 @@ import (
 	kubermaticlog "k8c.io/kubermatic/v2/pkg/log"
 )
 
-func newTestProjectReconciler(t *testing.T, objects []ctrlruntimeclient.Object, handlerFunc http.HandlerFunc) (*projectReconciler, *httptest.Server) {
+func newTestProjectReconciler(t *testing.T, objects []ctrlruntimeclient.Object, handler http.Handler) (*projectReconciler, *httptest.Server) {
 	dynamicClient := ctrlruntimefakeclient.
 		NewClientBuilder().
 		WithObjects(objects...).
 		Build()
-	ts := httptest.NewServer(handlerFunc)
+	ts := httptest.NewServer(handler)
 
 	grafanaClient := grafanasdk.NewClient(ts.URL, "admin:admin", ts.Client())
 
@@ -41,14 +43,21 @@ func newTestProjectReconciler(t *testing.T, objects []ctrlruntimeclient.Object, 
 }
 
 func TestProjectReconcile(t *testing.T) {
+	type request struct {
+		name     string
+		code     int
+		method   string
+		path     string
+		body     string
+		response string
+	}
 	testCases := []struct {
-		name          string
-		project       *kubermaticv1.Project
-		handlerFunc   http.HandlerFunc
-		requestName   string
-		requestCode   int
-		request       *grafanasdk.Org
-		statusMessage grafanasdk.StatusMessage
+		name         string
+		project      *kubermaticv1.Project
+		handlerFunc  http.HandlerFunc
+		requests     []request
+		hasFinalizer bool
+		err          bool
 	}{
 		{
 			name: "create org for project",
@@ -60,10 +69,106 @@ func TestProjectReconcile(t *testing.T) {
 					Name: "projectName",
 				},
 			},
-			requestName: "create",
-			requestCode: 200,
-			request: &grafanasdk.Org{
-				Name: "projectName-create",
+			hasFinalizer: true,
+			requests: []request{
+				{
+					name:     "create",
+					code:     200,
+					method:   "POST",
+					path:     "/api/orgs",
+					body:     `{"id":0,"name":"projectName-create","address":{"address1":"","address2":"","city":"","zipCode":"","state":"","country":""}}`,
+					response: `{"message": "org created", "OrgID": 1}`,
+				},
+			},
+		},
+		{
+			name: "create org for project - failed",
+			project: &kubermaticv1.Project{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "create",
+				},
+				Spec: kubermaticv1.ProjectSpec{
+					Name: "projectName",
+				},
+			},
+			hasFinalizer: true,
+			err:          true,
+		},
+		{
+			name: "delete org for project",
+			project: &kubermaticv1.Project{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:              "delete",
+					DeletionTimestamp: &metav1.Time{Time: time.Now()},
+				},
+				Spec: kubermaticv1.ProjectSpec{
+					Name: "projectName",
+				},
+			},
+			hasFinalizer: false,
+			requests: []request{
+				{
+					name:     "delete org by id",
+					code:     200,
+					method:   "DELETE",
+					path:     "/api/orgs/1",
+					response: `{}`,
+				},
+				{
+					name:     "get org by name",
+					code:     200,
+					method:   "GET",
+					path:     "/api/orgs/name/projectName-delete",
+					response: `{"id":1,"name":"projectName-create","address":{"address1":"","address2":"","city":"","zipCode":"","state":"","country":""}}`,
+				},
+			},
+		},
+		{
+			name: "delete org for project, get failed",
+			project: &kubermaticv1.Project{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:              "delete",
+					DeletionTimestamp: &metav1.Time{Time: time.Now()},
+					Finalizers:        []string{mlaFinalizer},
+				},
+				Spec: kubermaticv1.ProjectSpec{
+					Name: "projectName",
+				},
+			},
+			hasFinalizer: true,
+			err:          true,
+			requests: []request{
+				{
+					name:     "get org by name",
+					code:     404,
+					method:   "GET",
+					path:     "/api/orgs/name/projectName-delete",
+					response: `{"id":1,"name":"projectName-create","address":{"address1":"","address2":"","city":"","zipCode":"","state":"","country":""}}`,
+				},
+			},
+		},
+		{
+			name: "delete org for project delete failed",
+			project: &kubermaticv1.Project{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:              "delete",
+					DeletionTimestamp: &metav1.Time{Time: time.Now()},
+					Finalizers:        []string{mlaFinalizer},
+				},
+				Spec: kubermaticv1.ProjectSpec{
+					Name: "projectName",
+				},
+			},
+			hasFinalizer: true,
+			err:          true,
+			requests: []request{
+				{
+					name:     "get org by name",
+					code:     200,
+					method:   "GET",
+					path:     "/api/orgs/name/projectName-delete",
+					response: `{"id":1,"name":"projectName-create","address":{"address1":"","address2":"","city":"","zipCode":"","state":"","country":""}}`,
+				},
 			},
 		},
 	}
@@ -73,33 +178,42 @@ func TestProjectReconcile(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
 
-			handled := false
-			handlerFunc := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				decoder := json.NewDecoder(r.Body)
-				org := &grafanasdk.Org{}
-				if err := decoder.Decode(org); err != nil {
-					t.Fatalf("unmarshal grafana org failed: %v", err)
-				}
-				assert.Equal(t, tc.request, org)
-				w.WriteHeader(tc.requestCode)
-				encoder := json.NewEncoder(w)
-				encoder.Encode(tc.statusMessage)
-				handled = true
-			})
+			mux := http.NewServeMux()
+			handled := 0
+			for _, tcRequest := range tc.requests {
+				tcRequest := tcRequest
+				mux.HandleFunc(tcRequest.path, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+					assert.Equal(t, tcRequest.method, r.Method)
+					if tcRequest.body != "" {
+						decoder := json.NewDecoder(r.Body)
+						reqMap := map[string]interface{}{}
+						if err := decoder.Decode(&reqMap); err != nil {
+							t.Fatalf("unmarshal request failed: %v", err)
+						}
+						tcMap := map[string]interface{}{}
+						if err := json.Unmarshal([]byte(tcRequest.body), &tcMap); err != nil {
+							t.Fatalf("unmarshal expected map failed: %v", err)
+						}
+						assert.Equal(t, reqMap, tcMap)
+					}
+					w.WriteHeader(tcRequest.code)
+					fmt.Fprint(w, tcRequest.response)
+					handled++
+				}))
+			}
 			ctx := context.Background()
 			objects := []ctrlruntimeclient.Object{tc.project}
-			controller, server := newTestProjectReconciler(t, objects, handlerFunc)
-			request := reconcile.Request{NamespacedName: types.NamespacedName{Name: tc.requestName}}
-			if _, err := controller.Reconcile(ctx, request); err != nil {
-				t.Fatalf("reconciling failed: %v", err)
-			}
+			controller, server := newTestProjectReconciler(t, objects, mux)
+			request := reconcile.Request{NamespacedName: types.NamespacedName{Name: tc.project.Name}}
+			_, err := controller.Reconcile(ctx, request)
+			assert.Equal(t, tc.err, err != nil)
 			project := &kubermaticv1.Project{}
 			if err := controller.Get(ctx, request.NamespacedName, project); err != nil {
 				t.Fatalf("unable to get project: %v", err)
 			}
-			assert.True(t, kubernetes.HasFinalizer(project, mlaFinalizer))
-			assert.True(t, handled)
-			defer server.Close()
+			assert.Equal(t, tc.hasFinalizer, kubernetes.HasFinalizer(project, mlaFinalizer))
+			assert.Equal(t, handled, len(tc.requests))
+			server.Close()
 		})
 	}
 
